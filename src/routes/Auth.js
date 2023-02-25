a596762de5098678192eaaa7c328070f998d5b97
Byeunghee <choibh6258@hanyang.ac.kr>
2023-02-25 14:05:28 +0900

    Social Log In

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import { authService } from "fBase";
 import {
   createUserWithEmailAndPassword,
+  GoogleAuthProvider,
   signInWithEmailAndPassword,
+  signInWithPopup,
+  GithubAuthProvider
 } from "firebase/auth";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
-  const[error,setError] = useState("");
+  const [error, setError] = useState("");
   function onChange(event) {
     const {
       target: { name, value },
@@ -39,8 +42,22 @@ const Auth = () => {
     }
   };
   const toggleAccount = () => {
-    setNewAccount((prev)=>!prev);
-  }
+    setNewAccount((prev) => !prev);
+  };
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    const data = await signInWithPopup(authService, provider);
+    console.log(data);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -64,11 +81,15 @@ const Auth = () => {
         {error}
       </form>
       <span onClick={toggleAccount}>
-        {newAccount?"Sign In":"Create Account"}
+        {newAccount ? "Sign In" : "Create Account"}
       </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with GitHub</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with GitHub
+        </button>
       </div>
     </div>
   );
